487864d04976df0b73aab8dba00a28d02b96887a
Михаил <mmvlasko@mai.education>
2024-04-11 22:25:04 +0300

    feat: added method to get info about user activity
    Getting links to users and converting time from unix to a regular
    format are put into separate methods get_links_by_ids and convert_time.
    The user icon field has been added to the user data dictionary.
    The get_id_from_link method has added a restriction on
    analyzing only users (not groups).
    Added account privacy check (inability to get subscription data).
    Added the get_activity method for getting user activity
    by dates of comments and posts.

--- vkapi/tools.py
+++ vkapi/tools.py
@@ -1,6 +1,10 @@
 import re
 import vk_api
+
+from vk_api.exceptions import ApiError
+
 from datetime import datetime
+from time import time
 
 
 class Vk:
@@ -9,6 +13,7 @@ class Vk:
     def __init__(self, token: str) -> None:
         """
         Инициализация токена VK.
+
         :param token: str
         """
         self.__vk = vk_api.VkApi(token=token).get_api()
@@ -16,6 +21,7 @@ class Vk:
     def get_id_from_link(self, link: str) -> str:
         """
         Метод, возвращающий ID пользователя по ссылке на его профиль.
+
         :param link: str
         :return: str
         """
@@ -33,47 +39,23 @@ class Vk:
                 try:
                     resolved_name = self.__vk.utils.resolveScreenName(
                         screen_name=id_or_name[0])
-                    user_id = resolved_name['object_id'] if resolved_name['type'] != 'application' else None
+                    print(resolved_name)
+                    user_id = resolved_name['object_id'] if resolved_name['type'] == 'user' else None
                     if user_id is None:
                         raise TypeError
-                    if resolved_name['type'] == 'group':
-                        user_id = -user_id
                 except KeyError:
                     raise TypeError
+            print(type(user_id))
             return user_id
         else:
             raise TypeError
-
-    def get_info(self, link: str) -> dict:
-        """
-        Метод, возвращающий полную информацию о пользователе по ссылке.
-        :param link: str
-        :return: dict
-        """
-        return self.__vk.users.get(user_ids=self.get_id_from_link(link),
-                                   fields='photo_id, verified, sex, bdate, '
-                                          'city, country, home_town, has_photo, '
-                                          'photo_50, photo_100, photo_200_orig, '
-                                          'photo_200, photo_400_orig, photo_max, '
-                                          'photo_max_orig, online, lists, domain, '
-                                          'has_mobile, contacts, site, education, '
-                                          'universities, schools, status, last_seen, '
-                                          'followers_count, common_count, occupation, '
-                                          'nickname, relatives, relation, personal, '
-                                          'connections, exports, wall_comments, '
-                                          'activities, interests, music, movies, tv, '
-                                          'books, games, about, quotes, can_post, '
-                                          'can_see_all_posts, can_see_audio, '
-                                          'can_write_private_message, can_send_friend_request, '
-                                          'is_favorite, is_hidden_from_feed, timezone, screen_name, '
-                                          'maiden_name, crop_photo, is_friend, friend_status, '
-                                          'career, military, blacklisted, blacklisted_by_me')[0]
 
     @staticmethod
     def valid(key: str, obj: dict):
         """
         Метод, возвращающий значение по ключу словаря
-        или None в случае отсутствия введённого ключа ва словаре
+        или None в случае отсутствия введённого ключа в словаре
+
         :param key: str
         :param obj: dict
         :return: Any
@@ -82,37 +64,45 @@ class Vk:
             return obj[key]
         return None
 
-    def get_gg(self, link: str, items_count: int = 5) -> dict:
+    @staticmethod
+    def convert_time(times: list) -> list:
+        """
+        Метод, принимающий список моментов времени в формате Unix
+        и возвращающий список этих же моментов в формате ГГГГ-ММ-ДД ЧЧ:ММ:СС
+        :param times: list
+        :return: list
+        """
+        return [
+            datetime.fromtimestamp(
+                int(item['date'])
+            ).strftime('%Y-%m-%d %H:%M:%S') for item in times
+        ]
+
+    def get_info(self, link: str) -> dict:
         """
         Метод для получения подробных сведений о пользователе
         VK и возвращения словаря с ними
+
         :param link: str
-        :param items_count: int
         :return: dict
         """
         _id = self.get_id_from_link(link)
-        raw = self.__vk.users.get(user_id=_id, fields='first_name, last_name, bdate, country, city, activities, '
-                                                      'books, education, games, interests, movies, music, personal, '
-                                                      'relatives, counters')[0]
-        friends_ids = self.__vk.friends.get(user_id=_id, order='hints')
-        friends, sub_users, sub_groups = [], [], []
-
-        for elem in friends_ids['items'][:items_count]:
-            d = self.__vk.users.get(user_id=elem, fields='first_name, last_name')[0]
-            friends.append([d['first_name'], d['last_name'], f'https://vk.com/id{elem}'])
-
-        subs_ids = self.__vk.users.getSubscriptions(user_id=_id)
-        for elem in subs_ids['users']['items'][:items_count]:
-            d = self.__vk.users.get(user_id=elem, fields='first_name, last_name')[0]
-            sub_users.append([d['first_name'], d['last_name'], f'https://vk.com/id{elem}'])
-
-        for elem in subs_ids['groups']['items'][:items_count]:
-            d = self.__vk.groups.getById(group_id=elem, fields='name, screen_name')[0]
-            sub_groups.append([d['name'], f'https://vk.com/{d["screen_name"]}'])
-
-        posts = self.__vk.wall.get(owner_id=_id, count=100)
+        raw = self.__vk.users.get(user_id=_id, fields='first_name, last_name, bdate, '
+                                                      'country, city, activities, '
+                                                      'books, education, games, '
+                                                      'interests, movies, music, personal, '
+                                                      'relatives, counters, photo_50')[0]
+        try:
+            friends = self.__vk.friends.get(user_id=_id, order='hints')['items']
+            subs = self.__vk.users.getSubscriptions(user_id=_id)
+            sub_users = subs['users']['items']
+            sub_groups = subs['groups']['items']
+            posts = self.__vk.wall.get(owner_id=_id, count=100)
+        except ApiError:
+            friends = sub_users = sub_groups = posts = None
 
         data = {
+            'id': _id,
             'first_name': self.valid('first_name', raw),
             'last_name': self.valid('last_name', raw),
             'birthday': self.valid('bdate', raw),
@@ -131,16 +121,112 @@ class Vk:
                 'graduation': self.valid('graduation', raw)
             },
             'relatives': self.valid('relatives', raw),
-            'friends_count': int(raw['counters']['friends']),
-            'followers_count': int(raw['counters']['followers']),
+            'friends_count': self.valid('friends', raw['counters']),
+            'followers_count': self.valid('followers', raw['counters']),
             'friends': friends,
             'subscriptions': {
                 'users': sub_users,
                 'groups': sub_groups
             },
-            'post_dates': [datetime.fromtimestamp(
-                int(item['date'])
-            ).strftime('%Y-%m-%d %H:%M:%S') for item in posts['items']]
+            'post_dates': posts['items'],
+            'icon': self.valid('photo_50', raw)
         }
 
         return data
+
+    def get_links_by_ids(self, user_data: dict, count: tuple = (5, 5, 5)) -> dict:
+        """
+        Метод, принимающий словарь с данными о пользователе и кортеж с количествами
+        ссылок, которые необходимо получить для каждой категории,
+        и возвращающий словарь с именами и ссылками на друзей, аккаунты и группы,
+        на которые подписан пользователь('friends', 'users', 'groups' соответственно)
+
+        :param user_data: dict
+        :param count: tuple
+        :return: dict
+        """
+        friends, users, groups = [], [], []
+
+        if user_data['friends'] is not None:
+            for elem in user_data['friends'][:count[0]]:
+                d = self.__vk.users.get(user_id=elem, fields='first_name, last_name')[0]
+                friends.append([d['first_name'], d['last_name'], f'https://vk.com/id{elem}'])
+
+        if user_data['subscriptions']['users'] is not None:
+            for elem in user_data['subscriptions']['users'][:count[1]]:
+                d = self.__vk.users.get(user_id=elem, fields='first_name, last_name')[0]
+                users.append([d['first_name'], d['last_name'], f'https://vk.com/id{elem}'])
+
+        if user_data['subscriptions']['groups'] is not None:
+            for elem in user_data['subscriptions']['groups'][:count[2]]:
+                d = self.__vk.groups.getById(group_id=elem, fields='name, screen_name')[0]
+                groups.append([d['name'], f'https://vk.com/{d["screen_name"]}'])
+
+        return {
+            'friends': friends,
+            'users': users,
+            'groups': groups
+        }
+
+    def get_activity(self, user_data: dict, count: tuple = (40, 40, 40), time_limit: int = 2629743) -> list:
+        """
+        Метод, принимающих словарь с данными о пользователе и
+        возвращающий список с датами и временами публикаций постов
+        пользователем и оставлений им комментариев под постами
+        друзей, пользователей или групп, на которые он подписан.
+        Рассматриваются посты, выложенные не ранее, чем за
+        time_limit секунд дл текущего момента
+
+        :param user_data: dict
+        :param count: tuple
+        :param time_limit: int
+        :return: list
+        """
+        times = set()
+
+        if user_data['friends'] is not None:
+            for friend in user_data['friends'][:count[0]]:
+                try:
+                    posts = self.__vk.wall.get(owner_id=friend, count=100)
+                    for post in posts['items']:
+                        if post['date'] < time() - time_limit:
+                            break
+                        if post['comments']['count']:
+                            comments = self.__vk.wall.getComments(owner_id=friend, post_id=post['id'], count=100)
+                            for comment in comments['items']:
+                                if comment['from_id'] == user_data['id']:
+                                    times.add(comment['date'])
+                except ApiError:
+                    pass
+
+        if user_data['subscriptions']['users'] is not None:
+            for user in user_data['friends'][:count[1]]:
+                try:
+                    posts = self.__vk.wall.get(owner_id=user, count=100)
+                    for post in posts['items']:
+                        if post['date'] < time() - time_limit:
+                            break
+                        if post['comments']['count']:
+                            comments = self.__vk.wall.getComments(owner_id=user, post_id=post['id'], count=100)
+                            for comment in comments['items']:
+                                if comment['from_id'] == user_data['id']:
+                                    times.add(comment['date'])
+                except ApiError:
+                    pass
+
+        if user_data['subscriptions']['groups'] is not None:
+            for group in user_data['friends'][:count[2]]:
+                try:
+                    posts = self.__vk.wall.get(owner_id=-group, count=100)
+                    for post in posts['items']:
+                        if post['date'] < time() - time_limit:
+                            break
+                        if post['comments']['count']:
+                            comments = self.__vk.wall.getComments(owner_id=-group, post_id=post['id'], count=100)
+                            for comment in comments['items']:
+                                if comment['from_id'] == user_data['id']:
+                                    times.add(comment['date'])
+                except ApiError:
+                    pass
+
+        return sorted(list(times) + user_data['post_dates'])
