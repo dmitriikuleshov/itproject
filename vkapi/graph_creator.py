f44d65d2f2e8939aeb2ee10fd489350b8839544a
Dmitrii Kuleshov <dmkuleshov10@gmail.com>
2024-05-05 16:30:16 +0300

    feature/graphs added mutual-friends graph

--- vkapi/graph_creator.py
+++ vkapi/graph_creator.py
@@ -1,6 +1,7 @@
 from pyvis.network import Network
 from .tools import Vk, UserInfo
-from typing import List
+from typing import List, Tuple, Optional
+from copy import deepcopy
 import os
 
 
@@ -33,3 +34,42 @@ def create_friends_graph(link_to_save_graph: str, user_info: UserInfo) -> None:
 
     # saving graph
     graph.save_graph(link_to_save_graph)
+
+
+def create_mutual_friends_graph(link_to_save_graph: str,
+                                user_info: UserInfo,
+                                vk_mutual_friends_info: List[Tuple[UserInfo, Optional[List[UserInfo]]]]) -> None:
+    # create graph object
+    graph = Network(height='750px', width='700px', bgcolor='#222222', font_color='white')
+
+    graph.add_node(n_id=-1, label=f'{user_info["first_name"]} {user_info["last_name"]}', shape="circularImage",
+                   image=user_info["icon"], font={'size': 10}, size=25)
+
+    cur_id = 0
+    for friend_info in vk_mutual_friends_info:
+        graph.add_node(n_id=cur_id,
+                       label=f'{friend_info[0].get("first_name")} {friend_info[0].get("last_name")}',
+                       shape="circularImage",
+                       image=friend_info[0].get("icon"),
+                       font={"size": 10},
+                       size=15)
+        friend_id = deepcopy(cur_id)
+        graph.add_edge(-1, cur_id)
+        cur_id += 1
+
+        if friend_info[1] is not None:
+            for friend_friend_info in friend_info[1]:
+                graph.add_node(n_id=cur_id,
+                               label=f'{friend_friend_info.get("first_name")} {friend_friend_info.get("last_name")}',
+                               shape="circularImage",
+                               image=friend_friend_info.get("icon"),
+                               font={"size": 10},
+                               size=15)
+                graph.add_edge(friend_id, cur_id)
+                graph.add_edge(-1, cur_id)
+
+                cur_id += 1
+
+    # saving graph
+    graph.save_graph(link_to_save_graph)
+
--- vkapi/views.py
+++ vkapi/views.py
@@ -3,7 +3,7 @@ import os
 
 from .tools import Vk
 from main.models import VkAccount
-from .graph_creator import create_friends_graph
+from .graph_creator import create_friends_graph, create_mutual_friends_graph
 
 
 def user_info_view(request):
@@ -16,12 +16,19 @@ def user_info_view(request):
         vk = Vk(token=os.environ['VK_TOKEN'])
         try:
             vk_info = vk.get_info(link)
-            create_friends_graph('vkapi/templates/vkapi/friends-graph.html', vk_info)
+            vk_mutual_friends_info = vk.get_common_connections(link)
+
+            # create_friends_graph('vkapi/templates/vkapi/friends-graph.html',
+            #                     vk_info)
+            create_mutual_friends_graph('vkapi/templates/vkapi/mutual-friends-graph.html', vk_info,
+                                        vk_mutual_friends_info)
+
             response = render(request, 'vkapi/user-info.html', vk_info)
             if not VkAccount.objects.filter(link=link, creator=request.COOKIES['login']).exists():
                 VkAccount(link=link, creator=request.COOKIES['login']).save()
             return response
         except (TypeError, IndexError) as e:
+            print(e)
             return render(request, 'vkapi/user-info.html', {'error': True})
     else:
         return redirect('/')
